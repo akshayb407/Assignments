c52f7cab5630d5ac43385148e28102d664202db3
akshayb407 <akshayb407@gmail.com>
2022-06-11 14:52:51 +0530

    initial commit

---
+++ EmployeePayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
