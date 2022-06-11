c0ee229e35bab77cf85c533518fe92ae5af56c13
akshayb407 <akshayb407@gmail.com>
2022-06-11 15:07:00 +0530

    added UC1 solution

--- EmployeePayRoll/Program.cs
+++ EmployeePayRoll/Program.cs
@@ -6,7 +6,25 @@ namespace EmployeePayRoll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CheckEmployeePresentOrAbsent();
+
+
+        }
+         public static void CheckEmployeePresentOrAbsent()
+        {
+            Console.WriteLine("Welcome to Employee Pay Roll");
+            Random r = new Random();
+            int number = r.Next(0,2);
+            if (number == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
         }
     }
 }
