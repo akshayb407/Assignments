d14c43637d46789b4308d0aea0e74a99954f71b3
akshayb407 <akshayb407@gmail.com>
2022-06-11 20:12:39 +0530

    initial commit

--- EmployeePayRoll/Program.cs
+++ EmployeePayRoll/Program.cs
@@ -4,33 +4,42 @@ namespace EmployeePayRoll
 {
     internal class Program
     {
+        public const int IsPartTime = 1;
+        public const int IsFullTime = 2;
+        public const int IsAbsent = 0;
+
         static void Main(string[] args)
         {
-            CheckEmployeePresentOrAbsent();
+            int TotalWage = 0;
+            int WagePerHour = 20;
+            int PartTimeHour = 4;
+            int FullTimeHour = 8;
+            int NumberOfWorkingDays = 20;
 
-
-        }
-         public static void CheckEmployeePresentOrAbsent()
-        {
-            Console.WriteLine("Welcome to Employee Pay Roll");
-            Random r = new Random();
-            int wagePerHour = 20;
-            int fullDayhour = 8;
-            int totalWage = 0;
-            int number = r.Next(0,2);
-            if (number == 0)
+            for (int i = 0; i < NumberOfWorkingDays; i++)
             {
-                Console.WriteLine("Employee is Absent");
-              
+                Random random = new Random();
+                int number = random.Next(0, 3);
+
+                switch (number)
+                {
+                    case IsAbsent:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                    case IsFullTime:
+                        Console.WriteLine("Employee is fulltime");
+                        TotalWage = TotalWage + WagePerHour * FullTimeHour;
+                        break;
+                    case IsPartTime:
+                        Console.WriteLine("Employee is Present and PartTime Employee");
+                        TotalWage = TotalWage + WagePerHour * PartTimeHour;
+                        break;
+                }
+                    Console.WriteLine("Employee's total wage is:" + TotalWage);
+
 
             }
-            else
-            {
-                Console.WriteLine("Employee is Present");
-                totalWage = wagePerHour * fullDayhour;
-                Console.WriteLine("Employee's total wage is:" + totalWage);
 
-            }
         }
     }
 }
