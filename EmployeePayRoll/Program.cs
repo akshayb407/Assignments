a68540752dd312759088af3170c0475427f84ea5
akshayb407 <akshayb407@gmail.com>
2022-06-11 15:28:35 +0530

    added the UC2 Solution

--- EmployeePayRoll/Program.cs
+++ EmployeePayRoll/Program.cs
@@ -14,15 +14,21 @@ namespace EmployeePayRoll
         {
             Console.WriteLine("Welcome to Employee Pay Roll");
             Random r = new Random();
+            int wagePerHour = 20;
+            int fullDayhour = 8;
+            int totalWage = 0;
             int number = r.Next(0,2);
             if (number == 0)
             {
                 Console.WriteLine("Employee is Absent");
+              
 
             }
             else
             {
                 Console.WriteLine("Employee is Present");
+                totalWage = wagePerHour * fullDayhour;
+                Console.WriteLine("Employee's total wage is:" + totalWage);
 
             }
         }
